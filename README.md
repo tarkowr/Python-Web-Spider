57a39c29d435cffdd3fa0392c231025aa101ed18
Richie Tarkowski <tarkowr@gmail.com>
2020-03-14 17:32:30 -0400

    readme update and supporting multiple keywords

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Python Web Spider
+
+This Python script parses the HTTP response(s) from one or more URLs for key words.  
+I wrote this program for Applications of Information Security (ISI 312) at Ferris State University.
+
+## What I Learned
+* Sending HTTP Requests with Cookies
+* HTTP Response Parsing
+* File I/O in Python
+
+## Installation
+1. Install Python 2.7
+2. Set the URLs in the links.txt file, one URL per line
+3. Run this program `python web_spider.py`
--- web_spider.py
+++ web_spider.py
@@ -1,31 +1,34 @@
 import requests
 
 links_file = open("links.txt", "r")
-password_text_file = open("passwords.txt", "w")
+results_file = open("results.txt", "w")
 
 jar = requests.cookies.RequestsCookieJar()
-jar.set("PHPSESSID", "replace-with-auth-session-id", domain="192.168.0.2", path="/")
+jar.set("replace-with-session-name", "replace-with-auth-session-id", domain="replace-with-domain", path="/")
 
-query = "password"
-passwords_found = 0
+query = ["block", "matching"]
+number_found = 0
 context = 100  # 0 will write the entire http response to the passwords file.
 
 for url in links_file.readlines():
 	res = requests.get(url.strip(), cookies=jar)
 	res_text = res.text.strip().lower()
-	index = res_text.find(query)
-	if index != -1:
-		passwords_found = passwords_found + 1
-		password_text_file.write("Password found in %s \n" % url)
 
-		text_to_write = res_text
+	for word in query:
+		index = res_text.find(word)
 
-		if context != 0:
-			text_to_write = res_text[index-context:index+context]
+		if index != -1:
+			number_found = number_found + 1
+			results_file.write("\"%s\" found in %s \n" % (word.title(), url))
 
-		password_text_file.write(text_to_write + "\n\n")
+			text_to_write = res_text
 
-print("Number of password instances found: {}".format(passwords_found))
+			if context != 0:
+				text_to_write = res_text[index-context:index+context]
+
+			results_file.write(text_to_write + "\n\n")
+
+print("Number of instances found: {}".format(number_found))
 
 links_file.close()
-password_text_file.close()
+results_file.close()
